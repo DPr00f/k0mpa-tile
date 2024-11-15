7935b2992f611dc85d95b992df0c20244eeb069b
Joao Lopes <jocolopes@gmail.com>
2024-11-15 01:20:16 +0000

    Update position to the server

---
+++ coordinates.py
@@ -0,0 +1,32 @@
+import asyncio
+
+import requests
+
+from aiohttp import ClientSession
+
+from pytile import async_login
+
+import os
+
+EMAIL = os.environ.get("TILE_EMAIL")
+PASSWORD = os.environ.get("TILE_PASSWORD")
+API_PASSWORD = os.environ.get("TILE_API_PASSWORD")
+TILE_NAME = "k0mpass"
+API_URL = os.environ.get("TILE_API_SERVER", "http://127.0.0.1:3000/");
+
+
+async def main() -> None:
+    """Run!"""
+    async with ClientSession() as session:
+        api = await async_login(EMAIL, PASSWORD, session)
+
+        tiles = await api.async_get_tiles()
+
+        for tile_uuid, tile in tiles.items():
+            if tile.name != TILE_NAME:
+                break
+            r = requests.put(API_URL + "api/coordinates", json={'id':tile.name, 'latitude':tile.latitude, 'longitude':tile.longitude}, headers={ 'x-api-password': API_PASSWORD })
+            print(r)
+
+
+asyncio.run(main())
