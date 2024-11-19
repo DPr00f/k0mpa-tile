dd99f50385534c7b9055dcd2f42af699b9f90061
Joao Lopes <jocolopes@gmail.com>
2024-11-19 08:46:44 +0000

    Get location from phone

--- coordinates.py
+++ coordinates.py
@@ -9,6 +9,7 @@ EMAIL = os.environ.get("TILE_EMAIL")
 PASSWORD = os.environ.get("TILE_PASSWORD")
 API_PASSWORD = os.environ.get("TILE_API_PASSWORD")
 TILE_NAME = "k0mpass"
+TILE_TO_FIND = "iPhone de Carlos"
 API_URL = os.environ.get("TILE_API_SERVER", "http://127.0.0.1:3000/");
 
 post = False
@@ -25,13 +26,13 @@ async def main() -> None:
         tiles = await api.async_get_tiles()
 
         for tile_uuid, tile in tiles.items():
-            if tile.name != TILE_NAME:
-                break
+            if tile.name != TILE_TO_FIND:
+                continue
             if (post):
-                r = requests.post(API_URL + "api/coordinates", json={'id':tile.name, 'latitude':tile.latitude, 'longitude':tile.longitude}, headers={ 'x-api-password': API_PASSWORD })
+                r = requests.post(API_URL + "api/coordinates", json={'id':TILE_NAME, 'latitude':tile.latitude, 'longitude':tile.longitude}, headers={ 'x-api-password': API_PASSWORD })
                 print(r)
                 break
-            r = requests.put(API_URL + "api/coordinates", json={'id':tile.name, 'latitude':tile.latitude, 'longitude':tile.longitude}, headers={ 'x-api-password': API_PASSWORD })
+            r = requests.put(API_URL + "api/coordinates", json={'id':TILE_NAME, 'latitude':tile.latitude, 'longitude':tile.longitude}, headers={ 'x-api-password': API_PASSWORD })
             print(r)
 
 
