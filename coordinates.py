2797afa1b2b5f7f06da3532ee661d830bb7feb5a
Joao Lopes <jocolopes@gmail.com>
2024-11-20 12:18:18 +0000

    Check any tile that has the last timestamp

--- coordinates.py
+++ coordinates.py
@@ -3,13 +3,14 @@ import sys
 import requests
 from aiohttp import ClientSession
 from pytile import async_login
+from datetime import datetime
 import os
 
 EMAIL = os.environ.get("TILE_EMAIL")
 PASSWORD = os.environ.get("TILE_PASSWORD")
 API_PASSWORD = os.environ.get("TILE_API_PASSWORD")
 TILE_NAME = "k0mpass"
-TILE_TO_FIND = "iPhone de Carlos"
+TILES_TO_FIND = ["iPhone de Carlos", "k0mpass"]
 API_URL = os.environ.get("TILE_API_SERVER", "http://127.0.0.1:3000/");
 
 post = False
@@ -24,14 +25,22 @@ async def main() -> None:
         api = await async_login(EMAIL, PASSWORD, session)
 
         tiles = await api.async_get_tiles()
+        tile_to_find = None
+        last_timestamp = 0
 
         for tile_uuid, tile in tiles.items():
-            if tile.name != TILE_TO_FIND:
+            if  not (tile.name in TILES_TO_FIND):
                 continue
+            if datetime.timestamp(tile.last_timestamp) > last_timestamp:
+                tile_to_find = tile
+                last_timestamp = datetime.timestamp(tile.last_timestamp)
+
+        if (tile_to_find):
+            # print(f"Found tile {tile_to_find.name} at {tile_to_find.latitude}, {tile_to_find.longitude}")
             if (post):
                 r = requests.post(API_URL + "api/coordinates", json={'id':TILE_NAME, 'latitude':tile.latitude, 'longitude':tile.longitude}, headers={ 'x-api-password': API_PASSWORD })
                 print(r)
-                break
+                return
             r = requests.put(API_URL + "api/coordinates", json={'id':TILE_NAME, 'latitude':tile.latitude, 'longitude':tile.longitude}, headers={ 'x-api-password': API_PASSWORD })
             print(r)
 
