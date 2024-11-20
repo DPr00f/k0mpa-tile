568daf2c9b92cd1611177db41f758cc91cc9bbba
Joao Lopes <jocolopes@gmail.com>
2024-11-20 12:20:27 +0000

    Added debug

--- coordinates.py
+++ coordinates.py
@@ -14,10 +14,13 @@ TILES_TO_FIND = ["iPhone de Carlos", "k0mpass"]
 API_URL = os.environ.get("TILE_API_SERVER", "http://127.0.0.1:3000/");
 
 post = False
+debug = False
 
 for arg in sys.argv:
     if arg == "--post":
         post = True
+    if arg == "--debug":
+            debug = True
 
 async def main() -> None:
     """Run!"""
@@ -36,7 +39,8 @@ async def main() -> None:
                 last_timestamp = datetime.timestamp(tile.last_timestamp)
 
         if (tile_to_find):
-            # print(f"Found tile {tile_to_find.name} at {tile_to_find.latitude}, {tile_to_find.longitude}")
+            if debug:
+                print(f"Found tile {tile_to_find.name} at {tile_to_find.latitude}, {tile_to_find.longitude}")
             if (post):
                 r = requests.post(API_URL + "api/coordinates", json={'id':TILE_NAME, 'latitude':tile.latitude, 'longitude':tile.longitude}, headers={ 'x-api-password': API_PASSWORD })
                 print(r)
